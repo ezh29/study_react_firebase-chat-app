927fa16d77fdad8dec509142f822ab4ca4c23fa0
jh <ezh29@naver.com>
2021-09-27 14:10:35 +0900

    메시지 Header, Body, Form UI 만들기

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Form, ProgressBar, Row, Col, Button } from 'react-bootstrap'
 
 function MessageForm() {
     return (
         <div>
-            MessageForm
+            <Form>
+                <Form.Group>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+
+            <ProgressBar className="mt-2" now={60} label={`60%`} variant="warning" />
+
+            <Row className="mt-2">
+                <Col>
+                    <Button variant="primary" size="lg" style={{ width: '100%' }}>
+                        보내기
+                    </Button>
+                </Col>
+                <Col>
+                    <Button variant="primary" size="lg" style={{ width: '100%' }}>
+                        업로드
+                    </Button>
+                </Col>
+            </Row>
         </div>
     )
 }
--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Container, Row, Col, InputGroup, FormControl, Image, Accordion } from 'react-bootstrap'
+import { FaLock } from 'react-icons/fa'
+import { MdFavorite } from 'react-icons/md'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 function MessageHeader() {
+
+    const user = useSelector(state => state.user.currentUser)
+
+
+
     return (
-        <div>
-            MessageHeader
+        <div style={{ width: '100%', height: '170px', border: '.2rem solid @ececec', borderRadius: '4px', padding: '1rem', marginBottom: '1rem' }}>
+            <Container>
+                <Row>
+                    <Col> <h2><FaLock /> 방이름 <MdFavorite /></h2></Col>
+                    <Col>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1"><AiOutlineSearch /></InputGroup.Text>
+                            <FormControl
+                                placeholder="Search Messages"
+                                aria-label="Search"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <div style={{ display: 'flex', marginBottom: '1rem', alignItems: 'center', justifyContent: 'flex-end' }}>
+                    <Image src={user && user.photoURL}
+                        roundedCircle
+                        style={{ width: '30px', height: '30px', marginTop: '3px' }}
+                    />
+                    {user && user.displayName}
+                </div>
+                <Row>
+                    <Col>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                </Row>
+
+            </Container>
         </div>
     )
 }
