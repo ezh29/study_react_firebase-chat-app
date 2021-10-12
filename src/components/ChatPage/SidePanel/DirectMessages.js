1ac905ab69570c9a60177c72d71ce2c210778f7e
jihyun <ezh29@naver.com>
2021-10-12 16:08:17 +0900

    Side Panel Direct Message 기본 UI

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { FaRegSmile } from 'react-icons/fa'
 
-function DirectMessages() {
-    return (
-        <div>
-            DirectMessages
-        </div>
-    )
+export class DirectMessages extends Component {
+
+    renderDirectMessages = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <span style={{ display: 'flex', alignItems: 'center' }}>
+                    <FaRegSmile style={{ margin: 3 }} /> DirectMessages (1)
+                </span>
+                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                    {this.renderDirectMessages()}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default DirectMessages
